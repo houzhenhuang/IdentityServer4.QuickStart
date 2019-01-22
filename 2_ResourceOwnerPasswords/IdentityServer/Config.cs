27d9c84aa554924bf53bb30dad88f6d7c3ff02d0
hhz <1165071241@qq.com>
2019-01-22 18:52:21 +0800

    ResourceOwnerPasswords

---
+++ 2_ResourceOwnerPasswords/IdentityServer/Config.cs
@@ -0,0 +1,55 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        //交互式用户：以当前登录到系统的用户确定对象的身份(当前必须有用户登录到系统, 如果用户注销那么对象也会被销毁)
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client{
+                    ClientId = "client",
+                    AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
+                    //no interactive user, use the clientid/secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    //scopes that client has access to
+                    AllowedScopes={ "api1" }
+                }
+            };
+        }
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser> {
+                new TestUser{
+                    SubjectId="1001",
+                    Username="hhz",
+                    Password="password"
+                },
+                new TestUser{
+                    SubjectId="1002",
+                    Username="tom",
+                    Password="password"
+                }
+            };
+        }
+    }
+}
---
+++ 2_ResourceOwnerPasswords/IdentityServer/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // configure identity server with in-memory stores, keys, clients and resources
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients())
+                .AddTestUsers(Config.GetUsers());
+            //该AddTestUsers扩展方法做了以下几件事情:
+            //1.添加对资源所有者密码(ResourceOwnerPassword)授予的支持
+            //2.添加对登录UI通常使用的用户相关服务的支持（我们将在下一个快速入门中使用它）
+            //3.添加对基于测试用户的配置文件服务的支持（您将在下一个快速入门中了解更多信息）
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+    }
+}
