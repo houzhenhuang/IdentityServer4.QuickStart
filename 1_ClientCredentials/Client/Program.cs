bce6da821b72c422c21a9cccb524953a04eb8b91
hhz <1165071241@qq.com>
2019-01-22 18:50:33 +0800

    ClientCredentials

---
+++ 1_ClientCredentials/Client/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace Client
+{
+    class Program
+    {
+        /// <summary>
+        /// IdentityServer的令牌端点实现OAuth 2.0协议，您可以使用原始HTTP来访问它,但是，我们有一个名为IdentityModel的客户端库，它将协议交互封装在一个易于使用的API中。
+        /// </summary>
+        /// <param name="args"></param>
+        /// 
+        static void Main(string[] args) => MainAsync(args).Wait();
+
+        static async Task MainAsync(string[] args)
+        {
+            //discover endpoints from metadata
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+            //request token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+            Console.WriteLine(tokenResponse.Json);
+
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+            var response = await client.GetAsync("http://localhost:5001/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content =await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+            Console.ReadKey();
+        }
+    }
+}
